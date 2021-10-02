738f7daefc14f762abb9b158ec2780d3609b2168
James Adam Buckland <james.adam.buckland@gmail.com>
2021-10-02 10:18:49 -0400

    frequencies in toposort

--- toposort.py
+++ toposort.py
@@ -10,10 +10,13 @@ from typing import List, Text
 from src import card as card_lib
 from src import decomposer as decomposer_lib
 from src import toposorter as toposorter_lib
+from src import frequency as frequency_lib
 
 FLAGS = flags.FLAGS
 flags.DEFINE_string("xml_input_path", None,
                     "Path to the .xml file exported by Pleco.")
+flags.DEFINE_string("frequencies_csv_path", None,
+                    "Path to the frequencies csv, if available.")
 
 
 def main(argv):
@@ -21,6 +24,8 @@ def main(argv):
 
     if not FLAGS.xml_input_path:
         raise app.UsageError("Must provide --xml_input_path.")
+    if not FLAGS.frequencies_csv_path:
+        raise app.UsageError("Must provide --frequencies_csv_path.")
 
     cards_list = ET.parse(FLAGS.xml_input_path).getroot().find('cards')
     if cards_list is None:
@@ -40,9 +45,10 @@ def main(argv):
             continue
 
     TS = toposorter_lib.Toposorter(D, card_objects)
+    fq = frequency_lib.Frequencies(FLAGS.frequencies_csv_path)
 
     logging.info(f"Processed {len(card_objects)} cards.")
-    logging.info(TS.get_sorted())
+    logging.info(TS.get_sorted(key=lambda l: fq.get_frequency(l)))
 
 
 if __name__ == '__main__':
